e17476db21e3a7193b281002198a2157a3df384f
Marcus Netz <marcus.netz@godyo.com>
2020-11-24 21:57:49 +0100

    Formatting of Heads or Tails

--- src/commands/HeadsOrTails.js
+++ src/commands/HeadsOrTails.js
@@ -1,5 +1,5 @@
 const globals = require('../globals');
 module.exports = async (message, args, db) => {
 	const coin = Math.floor(Math.random() * 2);
-	message.reply('Folgende Seite hat der Münzwurf ergeben: ' + globals.Coin[coin] + '.');
+	message.reply('Die Münze bleibt auf **' + globals.Coin[coin] + '** liegen.');
 };
