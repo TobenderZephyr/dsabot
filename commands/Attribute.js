f5fd6a7e46ca83d3892d22098843b57cf57d27aa
TobenderZephyr <marcus@dre.ist>
2021-05-01 23:15:51 +0200

    fix attribute checks

--- commands/Attribute.js
+++ commands/Attribute.js
@@ -1,96 +1,86 @@
+require('module-alias/register');
 const globals = require('../globals');
-const {	CountOccurences } = require('@dsabot/CountOccurences');
-const { findMessage } = require('@dsabot/findMessage');
-const Random = require('random');
 const db = globals.db;
+const { roll } = require('@dsabot/Roll');
+const { findMessage } = require('@dsabot/findMessage');
+const { CompareResults } = require('@dsabot/CompareResults');
 
+function handleAttributeCheck(docs, { message, args }) {
+    let Attribute = isNaN(args[0])
+        ? HandleNamedAttributes({ Character: docs[0].character, args: args })
+        : null;
+    let Level = Attribute ? Attribute.Level : args[0] || 8;
+    let Bonus = parseInt(args[1]) || 0;
+    let dice = roll(2, 20, message.author.tag).dice;
+    const Result = CompareResults(dice, [Level, Level], Bonus);
+
+    // handle crits
+    if (Result.CriticalHit === 2) {
+        return message.reply(
+            `${findMessage('TITLE_CRIT_SUCCESS')}\n${findMessage('MSG_CRIT_SUCCESS')}`
+        );
+    }
+    if (Result.Fumbles === 2) {
+        return message.reply(
+            `${findMessage('TITLE_CRIT_FAILURE')}\n${findMessage('MSG_CRIT_FAILURE')}`
+        );
+    }
+
+    // every
+    if (dice[0] + Bonus > Level) {
+        return message.reply(
+            `Du hast die Probe (Stufe ${Level}) leider nicht bestanden 😢.\nDeine 🎲: ${dice[0]} ${
+                Bonus ? `+${Bonus}` : ''
+            }`
+        );
+    }
+    if (Attribute) {
+        return message.reply(
+            `Du hast die Probe auf ${Attribute.Name} (Stufe ${
+                Attribute.Level
+            }) bestanden. Deine 🎲: ${dice[0]} ${Bonus ? `+${Bonus}` : ''}`
+        );
+    }
+
+    return message.reply(
+        `Du hast die Probe (Stufe ${Level}) bestanden. Deine 🎲: ${dice[0]} ${
+            Bonus ? `+${Bonus}` : ''
+        }`
+    );
+}
+
+function HandleNamedAttributes({ Character: Character = [], args: args = [] } = {}) {
+    let Attribute = getAttribute(args[0]);
+    let Level = getAttributeLevel(Character, Attribute) || 8;
+
+    return {
+        Name: Attribute.name,
+        Level: Level,
+    };
+}
+
+function getAttributeLevel(Character = {}, Attribute = {}) {
+    return Character.attributes.find(attribute => attribute.id === Attribute.id).level;
+}
+
+function getAttribute(attribute = '') {
+    return attribute.length === 2
+        ? globals.Werte.find(a => a.kuerzel === attribute.toUpperCase())
+        : globals.Werte.find(a => a.name.toLowerCase() === attribute.toLowerCase());
+}
 module.exports = {
-	name: 'attribute',
-	description: '',
-	aliases: ['ap', 'ep'],
-	usage: '<Eigenschaft> / <Eigenschaftswert>',
-	needs_args: true,
-	async exec(message, args) {
-		try {
-			let Attribute;
-			let AttributeName;
-			let Level = 8;
-			await db.find({
-				user: message.author.tag,
-			}, async (err, docs) => {
-
-				// user calls with text, let's look him up in the database.
-				if (isNaN(args[0])) {
-					Attribute = HandleNamedAttributes({
-						Character: docs[0].character, 
-						args: args
-					});
-					AttributeName = Attribute.Name;
-					Level = Attribute.Level;
-				} else {
-					Level = args[0];
-				}
-				Random.use(message.author.tag);
-
-				const dice = [];
-				dice.push(Random.int(1, 20));
-				if (dice[0] == 1 || dice[0] == 20) {
-					dice.push(Random.int(1, 20));
-				}
-				// handle crits
-				if (CountOccurences(dice, 1) == 2) {
-					message.reply('Du hast einen kritischen Erfolg erzielt (' + dice.join(', ') + ')! 🎉🥳🎆');
-					return;
-				} else if (CountOccurences(dice, 20) == 2) {
-					message.reply('Du hast einen Patzer (' + dice.join(', ') + ')! 😭 Viel Erfolg beim nächsten mal!');
-					return;
-				}
-				if ((dice.length == 2 && dice[0] != 20 && dice[1] <= Level) || (dice.length == 1 && dice[0] <= Level)) {
-					if (AttributeName) {
-						message.reply('Du hast die Probe auf ' + AttributeName + ' (Stufe ' + Level + ') bestanden.\n' +
-							'Deine 🎲: ' + dice.join(', '));
-					} else {
-						message.reply('Du hast die Probe (Stufe ' + Level + ') bestanden.\n' +
-							'Deine 🎲: ' + dice.join(', '));
-					}
-				} else if (AttributeName) {
-					message.reply('Du hast die Probe auf ' + AttributeName + ' (Stufe ' + Level + ') leider nicht bestanden 😢.\n' +
-						'Deine 🎲: ' + dice.join(', '));
-				} else {
-					message.reply('Du hast die Probe (Stufe ' + Level + ') leider nicht bestanden 😢.\n' +
-						'Deine 🎲: ' + dice.join(', '));
-				}
-			});
-		} catch (e) {
-			throw e;
-		}
-	},
-};
-
-const HandleCrits = (dice) => {
-
-};
-
-const HandleNamedAttributes = ({Character: Character = [], args: args = []} = {}) => {
-
-	let Attributes = globals.Werte;
-	let Level = 8; // This is the minimum attributes value.
-	let AttributeName;
-	let AttributeId;
-
-	if (args[0].length == 2) {
-		AttributeId = Attributes.find(attribute => attribute.kuerzel === args[0].toUpperCase()).id;
-	} else {
-		AttributeId = args[0].toLowerCase() || 
-		Attributes.find(attribute => attribute.name.toLowerCase() === args[0].toLowerCase()).id;
-	}
-
-	Level = Character.attributes.find(attribute => attribute.id === AttributeId).level;
-	AttributeName = Attributes.find(attribute => attribute.id === AttributeId).name;
-
-	return {
-		Name: AttributeName, 
-		Level: Level
-	};
-
+    name: 'attribute',
+    description: '',
+    aliases: ['ap', 'ep'],
+    usage: '<Eigenschaft> / <Eigenschaftswert>',
+    needs_args: true,
+    async exec(message, args) {
+        db.find({ user: message.author.tag }, (err, docs) => {
+            if (err) {
+                message.reply(findMessage('ERROR'));
+                throw new Error(err);
+            }
+            handleAttributeCheck(docs, { message, args });
+        });
+    },
 };
