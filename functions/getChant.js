c46ccfef62fac1aa70fd5395c6162933786a87e1
Marcus Netz <marcus.netz@godyo.com>
2021-05-06 12:44:39 +0200

    coverage on getChant

--- functions/getChant.js
+++ functions/getChant.js
@@ -2,22 +2,17 @@ require('module-alias/register');
 const { getAttributeLevels } = require('@dsabot/getAttributeLevels');
 const Chants = require('@Lib/Chants.json');
 const { isEmpty } = require('@dsabot/isEmpty');
+
 const getChant = ({ Character: Character = [], chant_name: chantName = '' } = {}) => {
-    //if (!Character.hasOwnProperty('chants')) return;
+    if (!Character.hasOwnProperty('chants')) return null;
     const chantEntry =
         Chants.find(chant => chant.id.toLowerCase() === chantName.toLowerCase()) ||
         Chants.find(chant => chant.name.toLowerCase() === chantName.toLowerCase());
 
-    if (isEmpty(chantEntry)) {
-        console.log(`getChant() Did not find entry for ${chantName}`);
-        return null;
-    }
-
-    let Level = 0; // This is the minimum attributes value.
-    const Chant = Character.chants.find(chant => chant.id === chantEntry.id) || null;
-    if (Chant && Chant.hasOwnProperty('level')) {
-        Level = Chant.level || 0;
-    }
+    // let us filter out blessings.
+    if (isEmpty(chantEntry)) return null;
+    const Chant = Character.chants.find(chant => chant.id === chantEntry.id); // || null;
+    const Level = Chant.hasOwnProperty('level') ? Chant.level : 0;
     const Attributes = getAttributeLevels(chantEntry.attributes, Character);
 
     return { Name: chantEntry.name, Level: Level, Attributes: Attributes };
