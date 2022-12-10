03f3ace3111f39082bf263a8d3a6fb1fd78a48b9
TobenderZephyr <marcus@dre.ist>
2022-12-10 11:55:01 +0100

    Fix jest (#201)
    * fix got
    
    * fix jest

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,21 @@ module.exports = {
         '@data/(.*)': '<rootDir>/data/$i',
         '@Lib/(.*)': '<rootDir>/lib/$i',
     },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{js,jsx}',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+        '!**/.github/**',
+        '!**/__tests__/**',
+        '!**/__mocks__/**',
+    ],
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '<rootDir>/__tests__/',
+        '<rootDir>/.github/',
+        '<rootDir>/coverage/',
+        '<rootDir>/__mocks__/',
+    ],
+    coverageDirectory: 'coverage',
 };
